16182af9d53844ff2424fc21a6b5e1707f6aaf31
rjagtani <rjagtani@gmail.com>
2022-10-12 15:33:28 +0530

    called init of parent class

--- storage/batch_storage.py
+++ storage/batch_storage.py
@@ -1,5 +1,5 @@
 from .base_storage import BaseStorage
-from typing import List, Dict, Any, Optional
+from typing import Dict, Any, Optional
 
 
 class BatchStorage(BaseStorage):
@@ -8,11 +8,9 @@ class BatchStorage(BaseStorage):
 
     def __init__(self, store_targets=True):
         self.store_targets = store_targets
-        self._storage_x: List[Dict[str, Any]] = []
-        self._storage_y: List = []
+        super().__init__()
 
     def update(self, x: Dict, y: Optional[Any] = None):
         self._storage_x.append(x)
         if self.store_targets:
             self._storage_y.append(y)
-        self.stored_samples = len(self._storage_x)
