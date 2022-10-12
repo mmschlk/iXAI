8b6f710fe30e4bf88251ebf82024a000978ddff5
rjagtani <rjagtani@gmail.com>
2022-10-12 15:31:28 +0530

    changed imports

--- run_pfi.py
+++ run_pfi.py
@@ -2,20 +2,11 @@ from data.synthetic import SyntheticDataset
 import numpy as np
 from river.ensemble import AdaptiveRandomForestRegressor
 from river import metrics
-# from storage.sequence_storage import SequenceStorage
-# from storage.interval_storage import IntervalStorage
-# from storage.uniform_reservoir_storage import UniformReservoirStorage
-# from storage.geometric_reservoir_storage import GeometricReservoirStorage
-from storage.batch_storage import BatchStorage
-from imputer.marginal_imputer import MarginalImputer
+from storage import UniformReservoirStorage
+from imputer import DefaultImputer
 # from imputer.default_imputer import DefaultImputer
 from explainer.incremental_pfi import IncrementalPFI
 # from utils.converters import RiverToPredictionFunction
-
-
-storage = BatchStorage(store_targets=True)
-imputer = MarginalImputer(sampling_strategy='product')
-# imputer = DefaultImputer(values={'N_1': 5, 'N_2': 3, 'N_10': 2})
 
 
 def _model_function_dict(x_dict):
@@ -36,7 +27,7 @@ if __name__ == "__main__":
     #     return _dataset_target_function(x_list)
 
     TRAIN_TIME = 1000
-    TEST_TIME = 1010
+    TEST_TIME = 1030
 
     n_samples = 10000
     dataset = SyntheticDataset(n_numeric=_N_FEATURES, n_features=_N_FEATURES,
@@ -45,10 +36,16 @@ if __name__ == "__main__":
     stream = dataset.to_stream()
     feature_names = dataset.feature_names
     model = AdaptiveRandomForestRegressor()
-
     metric = metrics.MAE()
     feature_list = ['N_1', 'N_2', 'N_10']
     values = []
+
+    # Instantiating objects
+    storage = UniformReservoirStorage(store_targets=True, size=300)
+    # imputer = MarginalImputer(model, 'joint', storage)
+    imputer = DefaultImputer(model, values={'N_1': 5, 'N_2': 3, 'N_10': 2})
+    explainer = IncrementalPFI(model, feature_list, storage, imputer, 'mse')
+
     for (n, (x_i, y_i)) in enumerate(stream):
         n += 1
         y_i_pred = model.predict_one(x_i)
@@ -61,9 +58,9 @@ if __name__ == "__main__":
             break
 
     all_data = storage.get_data()
+    print(len(storage))
     print(len(all_data[0]))
     print(len(all_data[1]))
-    explainer = IncrementalPFI(model, feature_list, storage, imputer, 'mse')
 
     for (n, (x_i, y_i)) in enumerate(stream, (TRAIN_TIME + 1)):
         n += 1
