7d99b1116a237f80e3b5183058f48ceecc9971af
rjagtani <rjagtani@gmail.com>
2022-10-18 18:37:00 +0530

    fixed bug

--- increment_explain/storage/uniform_reservoir_storage.py
+++ increment_explain/storage/uniform_reservoir_storage.py
@@ -18,14 +18,16 @@ class UniformReservoirStorage(ReservoirStorage):
             store_targets=store_targets
         )
         super(ReservoirStorage, self).__init__()
+        self.stored_samples: int = 0
 
     def update(self, x: Dict, y: Optional[Any] = None):
+        self.stored_samples += 1
         if len(self._storage_x) < self.size:
             self._storage_x.append(x)
             if self.store_targets:
                 self._storage_y.append(y)
         else:
-            random_integer = random.randint(1, len(self._storage_x))
+            random_integer = random.randint(1, self.stored_samples)
             if random_integer <= self.size:
                 rand_idx = random.randrange(self.size)
                 self._storage_x[rand_idx] = x
