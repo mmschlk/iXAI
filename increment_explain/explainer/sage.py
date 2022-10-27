76ab5388b181a1f50c71a765818fe09e743b9208
Maximilian <Maximilian.Muschalik@lmu.de>
2022-10-27 16:39:53 +0200

    created an interval and batch sage explainer with storage and imputers

--- increment_explain/explainer/sage.py
+++ increment_explain/explainer/sage.py
@@ -13,7 +13,7 @@ from tqdm import tqdm
 
 from increment_explain.explainer import BaseIncrementalFeatureImportance
 from increment_explain.imputer import MarginalImputer, BaseImputer
-from increment_explain.storage import GeometricReservoirStorage, UniformReservoirStorage, IntervalStorage
+from increment_explain.storage import GeometricReservoirStorage, UniformReservoirStorage, IntervalStorage, BatchStorage
 from increment_explain.storage.base import BaseStorage
 
 
@@ -50,15 +50,15 @@ class IncrementalSageExplainer(BaseIncrementalFeatureImportance):
             dynamic_setting=dynamic_setting,
             smoothing_alpha=smoothing_alpha,
         )
-        self.storage = storage
-        if storage is None:
-            if dynamic_setting:
-                self.storage = GeometricReservoirStorage(store_targets=False, size=100)
-            else:
-                self.storage = UniformReservoirStorage(store_targets=False, size=100)
         self._loss_function = loss_function
+        self.storage = storage
         self.imputer = imputer
         if imputer is None:
+            if storage is None:
+                if dynamic_setting:
+                    self.storage = GeometricReservoirStorage(store_targets=False, size=100)
+                else:
+                    self.storage = UniformReservoirStorage(store_targets=False, size=100)
             self.imputer = MarginalImputer(self.model_function, 'product', storage)
         self.n_inner_samples = n_inner_samples
         self._pfi_mode = pfi_mode
@@ -95,26 +95,66 @@ class BatchSageExplainer:
             feature_names: list[str],
             loss_function: Callable,
             n_inner_samples: int = 1,
+            storage: BaseStorage = None,
+            imputer: BaseImputer = None,
     ):
         self.feature_names = feature_names
         self.model_function = model_function
         self._loss_function = loss_function
-        self._sub_sample_size = n_inner_samples
+        self._n_inner_samples = n_inner_samples
         self._loss_function = loss_function
-
+        self.storage = storage
+        if storage is None:
+            self.storage = BatchStorage(store_targets=True)
+        self.imputer = imputer
+        if imputer is None:
+            self.imputer = MarginalImputer(
+                sampling_strategy='joint', model_function=self.model_function, storage_object=storage
+            )
         self.importance_values = {feature_name: 0. for feature_name in self.feature_names}
 
+    def update_storage(self, x_i, y_i):
+        self.storage.update(x=x_i, y=y_i)
+
     def explain_one(self, x_i, y_i):
-        raise NotImplementedError("Use 'explain_many' function.")
+        self.storage.update(x_i, y_i)
+        x_data, y_data = self.storage.get_data()
+        self.explain_many(x_data=x_data, y_data=y_data)
+        return self.importance_values
 
     def explain_many(
             self,
             x_data: list[dict[str, Any]],
             y_data: list[Any],
-            sub_sample_size: Optional[int] = None
     ) -> dict[str, float]:
-        if sub_sample_size is None:
-            sub_sample_size = self._sub_sample_size
+        sage_values = {feature: 0. for feature in self.feature_names}
+        n_data = len(x_data)
+        marginal_prediction = sum(self.model_function(x_data)) / n_data
+        for n, (x_i, y_i) in tqdm(enumerate(zip(x_data, y_data), start=1), total=n_data):
+            permutation_chain = np.random.permutation(self.feature_names)
+            loss_previous = self._loss_function(y_true=y_i, y_prediction=marginal_prediction)
+            features_not_in_S = set(self.feature_names)
+            for feature in permutation_chain:
+                features_not_in_S.remove(feature)
+                predictions = self.imputer.impute(
+                    feature_subset=features_not_in_S,
+                    x_i=x_i,
+                    n_samples=self._n_inner_samples
+                )
+                y = np.mean(predictions)
+                feature_loss = self._loss_function(y_true=y_i, y_prediction=y)
+                marginal_contribution = loss_previous - feature_loss
+                sage_values[feature] += marginal_contribution
+                loss_previous = feature_loss
+            n_data = n
+        self.importance_values = {feature: sage_value / n_data for feature, sage_value in sage_values.items()}
+        return self.importance_values
+
+    def explain_many_original(
+            self,
+            x_data: list[dict[str, Any]],
+            y_data: list[Any],
+    ) -> dict[str, float]:
         sage_values = {feature: 0. for feature in self.feature_names}
         n_data = len(x_data)
         marginal_prediction = sum(self.model_function(x_data)) / n_data
@@ -125,7 +165,7 @@ class BatchSageExplainer:
             for feature in permutation_chain:
                 x_S[feature] = x_i[feature]
                 y = 0
-                for k in range(1, sub_sample_size + 1):
+                for k in range(1, self._n_inner_samples + 1):
                     x_marginal = x_data[random.randint(0, n_data - 1)]
                     x_marginal = {**x_marginal, **x_S}
                     y += self.model_function(x_marginal)[0]
@@ -139,27 +179,35 @@ class BatchSageExplainer:
         return self.importance_values
 
 
-class IntervalSageExplainer:
+class IntervalSageExplainer(BatchSageExplainer):
 
     def __init__(
             self,
             model_function: Callable,
             feature_names: list[str],
             loss_function: Callable,
-            sub_sample_size: int = 1,
-            normalization_mode: Optional[str] = None,
-            interval_length: int = 100
+            n_inner_samples: int = 1,
+            interval_length: int = 100,
+            storage: BaseStorage = None,
+            imputer: BaseImputer = None,
     ):
-        self.model_function = model_function
-        self.feature_names = feature_names
-        self._loss_function = loss_function
-        self._sub_sample_size = sub_sample_size
-        self._normalization_mode = normalization_mode
-        self._loss_function = loss_function
-        self._storage = IntervalStorage(store_targets=True, size=interval_length)
-        self._interval_length = interval_length
 
-        self.importance_values = {feature: 0. for feature in self.feature_names}
+        if storage is None:
+            storage = IntervalStorage(store_targets=True, size=interval_length)
+
+        if imputer is None:
+            imputer = MarginalImputer(model_function=model_function, sampling_strategy='joint', storage_object=storage)
+
+        super().__init__(
+            model_function=model_function,
+            feature_names=feature_names,
+            loss_function=loss_function,
+            n_inner_samples=n_inner_samples,
+            storage=storage,
+            imputer=imputer
+        )
+        self._interval_length = interval_length
+        self.seen_samples = 0
 
     def explain_one(
             self,
@@ -168,32 +216,10 @@ class IntervalSageExplainer:
             sub_sample_size: Optional[int] = None,
             force_explain: bool = False
     ) -> dict[str, float]:
-        self._storage.update(x=x_i, y=y_i)
+        self.storage.update(x=x_i, y=y_i)
         self.seen_samples += 1
         if not force_explain and self.seen_samples % self._interval_length != 0:
             return self.importance_values
-        sage_values = {feature: 0. for feature in self.feature_names}
-        if sub_sample_size is None:
-            sub_sample_size = self._sub_sample_size
-        x_data, y_data = self._storage.get_data()
-        n_data = len(x_data)
-        marginal_prediction = sum(self.model_function(x_data)) / n_data
-        for n, (x_i, y_i) in enumerate(zip(x_data, y_data), start=1):
-            permutation_chain = np.random.permutation(self.feature_names)
-            x_S = {}
-            loss_previous = self._loss_function(y_true=y_i, y_prediction=marginal_prediction)
-            for feature in permutation_chain:
-                x_S[feature] = x_i[feature]
-                y = 0
-                for k in range(1, sub_sample_size + 1):
-                    x_marginal = x_data[random.randint(0, n_data - 1)]
-                    x_marginal = {**x_marginal, **x_S}
-                    y += self.model_function(x_marginal)[0]
-                y /= k
-                feature_loss = self._loss_function(y_true=y_i, y_prediction=y)
-                marginal_contribution = loss_previous - feature_loss
-                sage_values[feature] += marginal_contribution
-                loss_previous = feature_loss
-            n_data = n
-        self.importance_values = {feature: sage_value / n_data for feature, sage_value in sage_values.items()}
+        x_data, y_data = self.storage.get_data()
+        super().explain_many(x_data=x_data, y_data=y_data)
         return self.importance_values
