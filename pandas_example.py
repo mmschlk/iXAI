6bc02c82e20f04b6fbf8fb915066f65ff0621408
Maximilian <Maximilian.Muschalik@lmu.de>
2022-12-02 19:20:00 +0100

    added readme, initial experiments, and small requirements.txt.

--- pandas_example.py
+++ pandas_example.py
@@ -4,43 +4,95 @@ from river.stream import iter_pandas
 from river.ensemble import AdaptiveRandomForestRegressor
 from river.metrics import MSE
 from river.utils import Rolling
-
-import matplotlib.pyplot as plt
+from river import preprocessing, compose
 
 from sklearn.datasets import fetch_california_housing
 from sklearn.utils import shuffle
+
+from increment_explain.explainer import IncrementalPFI
+from increment_explain.explainer.sage import IncrementalSage, IntervalSage
+from increment_explain.imputer import MarginalImputer
+from increment_explain.storage import GeometricReservoirStorage
+from increment_explain.utils.wrappers import RiverToPredictionFunction
 
 if __name__ == "__main__":
 
     data = fetch_california_housing()
     data_x = pd.DataFrame(data['data'], columns=data['feature_names'])
     data_y = pd.Series(data['target'], name='target')
+    data_y = data_y - data_y.mean() / (data_y.max() - data_y.min())
     data_x, data_y = shuffle(data_x, data_y)  # of course don't shuffle real stream data
 
+    feature_names = data['feature_names']
+
     # model
-    model = AdaptiveRandomForestRegressor(n_models=50)  # most important hyperparameter is n_models
+    model = AdaptiveRandomForestRegressor(n_models=15)  # most important hyperparameter is n_models
+    model = compose.Pipeline(preprocessing.StandardScaler(), model)
+    model_function = RiverToPredictionFunction(model.predict_one)
 
-    # performance metric
-    metric = MSE()  # real metric can be everything from river.metrics
-    metric_rolling = Rolling(metric, window_size=200)  # metric wrapper to summarize individual prediction losses
+    # performance metric and loss
+    loss_metric = MSE()  # real metric can be everything from river.metrics used for explanations
+    training_metric = Rolling(MSE(), window_size=1000) # metric wrapper to summarize individual prediction losses
 
-    model_performance = []  # for plotting
+    # Get imputer and explainers ---------------------------------------------------------------------------------------
+    storage = GeometricReservoirStorage(
+        size=200,
+        store_targets=False
+    )
+
+    imputer = MarginalImputer(
+        model_function=model_function,
+        storage_object=storage,
+        sampling_strategy="joint"
+    )
+
+    incremental_sage = IncrementalSage(
+        model_function=model_function,
+        loss_function=loss_metric,
+        imputer=imputer,
+        storage=storage,
+        feature_names=feature_names,
+        smoothing_alpha=0.001,
+        n_inner_samples=1
+    )
+
+    interval_sage = IntervalSage(
+        model_function=model_function,
+        loss_function=loss_metric,
+        feature_names=feature_names,
+        interval_length=2000,
+        n_inner_samples=1
+    )
+
+    incremental_pfi = IncrementalPFI(
+        model_function=model_function,
+        loss_function=loss_metric,
+        imputer=imputer,
+        storage=storage,
+        feature_names=feature_names,
+        smoothing_alpha=0.001,
+        n_inner_samples=1
+    )
 
     # main training loop data_x and data_y needs to be pd.DataFrames and / or pd.Series
     for n, (x_i, y_i) in enumerate(iter_pandas(data_x, data_y), start=1):
         y_i_pred = model.predict_one(x_i)
-        metric_rolling.update(y_true=y_i, y_pred=y_i_pred)
-        model_performance.append(metric.get())
-        model.learn_one(x_i, y_i)
-        if n % 1000 == 0:
-            print(f"{n}: performance {metric_rolling.get()}\n"
-                  f"{n}: y_i         {y_i}\n"
-                  f"{n}: y_i_pred    {y_i_pred}\n"
-                  f"{n}: x_i         {x_i}\n")
+        training_metric.update(y_true=y_i, y_pred=y_i_pred)
 
-    plt.plot(model_performance)
-    plt.ylim((0, 1))
-    plt.title(f"Model Performance")
-    plt.xlabel(f"Time / Samples")
-    plt.ylabel(f"Performance")
-    plt.show()
+        # sage inc
+        _ = incremental_sage.explain_one(x_i, y_i)
+        _ = interval_sage.explain_one(x_i, y_i)
+        _ = incremental_pfi.explain_one(x_i, y_i, update_storage=False)
+
+        model.learn_one(x_i, y_i)
+
+        if n % 1000 == 0:
+            print(f"{n}: perf                {training_metric.get()}\n"
+                  f"{n}: x_i, y_i            {x_i, y_i}\n"
+                  f"{n}: inc-sage            {incremental_sage.importance_values}\n"
+                  f"{n}: int-sage            {interval_sage.importance_values}\n"
+                  f"{n}: pfi                 {incremental_pfi.importance_values}\n"
+                  f"{n}: diff                {incremental_sage.marginal_loss - incremental_sage.model_loss}\n"
+                  f"{n}: marginal-loss       {incremental_sage.marginal_loss}\n"
+                  f"{n}: model-loss          {incremental_sage.model_loss}\n"
+                  f"{n}: sum-sage            {sum(list(incremental_sage.importance_values.values()))}\n")
