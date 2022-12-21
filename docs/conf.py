600758485baa5cabe8a5b0564e6156acb033722b
rohit <rjagtani@gmail.com>
2022-12-21 13:50:00 +0100

    added path for imports

--- docs/conf.py
+++ docs/conf.py
@@ -6,11 +6,8 @@
 from sphinx.builders.html import StandaloneHTMLBuilder
 import os
 import sys
+sys.path.insert(0, os.path.abspath("../"))
 import ixai
-
-
-sys.path.insert(0, os.path.abspath("../.."))
-
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
