de849c33d45f87fc5b0b3e612556f8e2929c7e7c
Maximilian <maximilian.muschalik@gmail.com>
2023-02-04 16:56:56 +0100

    add docs to DefaultImputer

--- ixai/imputer/default_imputer.py
+++ ixai/imputer/default_imputer.py
@@ -4,6 +4,12 @@ from .base import BaseImputer
 
 
 class DefaultImputer(BaseImputer):
+    """Imputer that always imputes with default values (e.g. mean values).
+
+    Args:
+        model_function (Callable): Model function to impute values for.
+        values (dict): Mapping from feature names to default values for the features.
+    """
     def __init__(self, model_function: Callable, values: dict):
         self.values = values
         super().__init__(
@@ -11,6 +17,14 @@ class DefaultImputer(BaseImputer):
         )
 
     def impute(self, feature_subset: list, x_i: dict, n_samples: int = 1):
+        """Imputes a subset of features with default values
+
+        Args:
+            feature_subset (list): Feature names to impute.
+            x_i (dict): Instance to impute values in.
+            n_samples (int): Number of imputed instances to create (effectively cloning it).
+                Defaults to 1.
+        """
         sampled_values = {feature: self.values[feature] for feature in feature_subset}
         prediction = self.model_function({**x_i, **sampled_values})
         prediction = [prediction for _ in range(n_samples)]
