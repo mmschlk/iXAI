33744b12e2a345c9d19a925e6ad1c64f19086076
rohit <rjagtani@gmail.com>
2023-09-14 17:16:44 +0200

    added pdp to init

--- ixai/explainer/__init__.py
+++ ixai/explainer/__init__.py
@@ -4,10 +4,13 @@ This modul gathers the explainers.
 
 from .pfi import IncrementalPFI
 from .sage import IncrementalSage, BatchSage, IntervalSage
+from .pdp import IncrementalPDP, BatchPDP
 
 __all__ = [
     "IncrementalPFI",
     "IncrementalSage",
     "BatchSage",
-    "IntervalSage"
+    "IntervalSage",
+    "IncrementalPDP",
+    "BatchPDP"
 ]
